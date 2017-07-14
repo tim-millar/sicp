fd83433d2a2f09c51f22e56239c9c7254f9f8262
Tim Millar <timothy.millar@onthebeach.co.uk>
2017-07-14 14:42:17 +0100

    Exercise 1.33

--- ch1/exercises_ch1-3.rkt
+++ ch1/exercises_ch1-3.rkt
@@ -82,5 +82,34 @@
   (define (iter a result)
     (if (> a b)
         result
-        (iter (next a) (* a result))))
+        (iter (next a) (* (term a) result))))
   (iter a 1))
+
+;; ========================================
+;; Exercise 1.33
+;; ========================================
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (iter-accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+(define (sum-acc term a next b)
+  (accumulate + 0 term a next b))
+
+(define (prod-acc term a next b)
+  (accumulate * 1 term a next b))
+
+(define (sum-iter-acc term a next b)
+  (iter-accumulate + 0 term a next b))
+
+(define (prod-iter-acc term a next b)
+  (iter-accumulate * 1 term a next b))
